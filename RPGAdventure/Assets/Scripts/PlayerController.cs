204851f67dad32dd5e51a1db1b49eaba53012575
sadaniyaj07 <sadaniyaj07@gmail.com>
2025-03-02 23:39:26 -0500

    game created

---
+++ RPGAdventure/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    private bool isMoving;
+    private Vector2 input;
+
+    private Animator animator;
+
+    public LayerMask solidObjectLayers;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving)
+        {
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            animator.SetFloat("moveX", input.x);
+            animator.SetFloat("moveY", input.y);
+
+
+            if (input.x != 0) input.y = 0;
+
+            if (input != Vector2.zero)
+            {
+                animator.SetFloat("moveX", input.x);
+                animator.SetFloat("moveY", input.y);
+
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                if(IsWalkable(targetPos))
+                     StartCoroutine(Move(targetPos));
+                //Debug.Log("Original Pos: " + transform.position);
+                //Debug.Log("Target Pos: " + targetPos);
+            }
+        }
+        //animator.SetBool("isMoving", isMoving); 
+    }
+
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+        while ((targetPos - transform.position).sqrMagnitude > Mathf.Epsilon)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+        transform.position = targetPos;
+        isMoving = false;
+    }
+
+    private bool IsWalkable(Vector3 targetPos)
+    {
+        if (Physics2D.OverlapCircle(targetPos, 0.2f, solidObjectLayers) != null)
+    {
+            return false;
+           
+    }
+            return true;
+    }
+       
+}
+
